6608a719cf1a91a4d86d20230f79a0abe2889884
RonRain <rarainero@gmail.com>
2024-02-13 11:11:49 -0500

    add app

--- fire-safety-guard/src/App.js
+++ fire-safety-guard/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+//css
+import './App.css'
+
+import { Route, Routes, useLocation } from 'react-router-dom'
+
+//pages
+import Details from './pages/Details/Details'
+import About from './pages/About/About'
+import ContactForm from './pages/ContactForm/ContactForm'
+
+import NavBar from './components/NavBar/NavBar'
+import Footer from './components/Footer/Footer'
 
 function App() {
+  const location = useLocation()
+
+  const isRootPath = location.pathname === '/'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <NavBar />
+      {/* <a href="tel:+3473927031" className="text-center" style={{}}><strong>For Immediate Assistance Call 3473927031</strong></a> */}
+      <Routes>
+        <Route path='/details' element={<Details />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<ContactForm />} />
+      </Routes>
+      {isRootPath && (
+      <main className="container">
+      <div className="row">
+        <div className="col-lg-8 mx-auto">
+          <h1 className="display-4 text-center">OSHA 2020 Consultants</h1>
+          <img src="./fire-safety.png" alt="At Home Fire Safety" className="fire-safety img-fluid" style={{ float:'right', maxWidth: '65%', height: 'auto' }} />
+          <p className="lead">
+            OSHA 2020 Consultants is your trusted partner for fire safety solutions. Serving the tri-state area, we specialize in a wide range of services to ensure the safety and compliance of your premises.
+          </p>
+          <p className='lead'><strong>All of our Fire Guards are FDNY certified.</strong> Our fire guards can provide assistance for Impairments (F-01), Shelters (F-02), Temporary Assembly (F-04), Fire Safety Managers (S-56) and Temporary Heat (S-92). We have worked for corporate buildings, venues construction sites and many more. Whatever you need, we can provide you with the highest level of service.</p>
+          <p className='lead'>We offer round-the-clock availability, <strong>24/7/365</strong>, to address any Fire Life Safety emergencies at your facility, ensuring compliance with local regulations and the continuous operation of your building.</p>
+        </div>
+      </div>
+    </main>
+      )}
+    {/* <Footer /> */}
+    </>
+  )
 }
 
-export default App;
+export default App
