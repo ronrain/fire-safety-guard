47915938ef9dc4d141dbfd0f59bfd788ef80ee27
RonRain <rarainero@gmail.com>
2024-02-16 14:08:05 -0500

    update navbar

--- fire-safety-guard/src/components/NavBar/NavBar.jsx
+++ fire-safety-guard/src/components/NavBar/NavBar.jsx
@@ -7,38 +7,38 @@ const NavBar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         {/* Left side of the navbar */}
-        <div className="navbar-left navbar-nav me-auto mb-2 mb-lg-0">
-          <a className="navbar-brand" href="/">
-            <img src="./logo.png" alt="Logo" className="navbar-brand " style={{ width: '70px', height: 'auto', textDecoration: 'none', textDecorationLine: 'none', color: '#222725' }} />
+        <div className="d-flex align-items-center">
+          <a className="navbar-brand me-3" href="/">
+            <img src='./logo.png' alt="Logo" style={{ width: '70px', height: 'auto' }} />
           </a>
-          <a href="tel:+3473927031" className="phone-number mr-3" style={{color: '#3185fc', fontSize:'1.8rem'}}>
-            <strong>For Immediate Assistance <br /> Call 3473927031</strong>
-          </a>
+        {/* Center content */}
+        <h3 href="tel:+3473927031" className="navbar-text text-center mb-0 phone-number me-auto" style={{ color: '#3185fc' }}><strong>In Case of an Emergency <br />Call 3473927031</strong></h3>
         </div>
         {/* Right side of the navbar */}
-        <ul class="nav justify-content-center">
-  <li class="nav-item">
-    <h1 class="nav-text" style={{color: '#3185fc'}}>OSHA 2020 Consultants</h1>
-  </li>
-  </ul>
-        <div className="navbar-right nav justify-content-end">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0" style={{fontSize:'1.8rem', color:'#3185fc'}}>
-            <li className="nav-item">
-              <Link to="/about" className="nav-link ms-auto mb-2 mb-lg-0" style={{color: '#3185fc'}}>
-                Who We Are
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/details" className="nav-link ms-auto mb-2 mb-lg-0" style={{color: '#3185fc'}}>
-                What We Do
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/contact" className="nav-link ms-auto mb-2 mb-lg-0" style={{color: '#3185fc'}}>
-                Contact Us
-              </Link>
+        <div className="d-flex align-items-center">
+          {/* Links dropdown menu */}
+          <div className="dropdown justify-content-end">
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+            OSHA 2020 Consultants
+            </button>
+            <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+              <li className="nav-item">
+                <Link to="/about" className="nav-link" style={{ color: '#3185fc' }}>
+                  Who We Are
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/details" className="nav-link" style={{ color: '#3185fc' }}>
+                  What We Do
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link" style={{ color: '#3185fc' }}>
+                  Contact Us
+                </Link>
             </li>
           </ul>
+        </div>
         </div>
       </div>
     </nav>
